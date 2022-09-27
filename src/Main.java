cc92ac1a891f73c0e093fbb91563dfecbf5eff40
Zeshan Shahid <z.shahid@hotmail.de>
2022-09-27 14:09:09 +0200

    clean up code

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,25 @@ public class Main {
         System.out.println("Moin");
         System.out.println(subtract(9, 4));
         System.out.println(sum(9, 4));
+        System.out.println(sumString(5, 16));
 
     }
+
     public static int subtract(int value1, int value2) {
         return value1 - value2;
     }
 
     public static int sum(int value1, int value2) {
         return value1 + value2;
+
+
+    }
+
+
+    public static String sumString(int value1, int value2) {
+        int result = value1 + value2;
+
+
+        return "Summe: " + result;
     }
 }
