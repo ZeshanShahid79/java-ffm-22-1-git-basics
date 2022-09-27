6a980b99637886a3885cccb0310d316b1d89d63d
chris_yooo <hhxtreams@gmail.com>
2022-09-27 12:23:55 +0200

    changed lines

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Moin");
-        System.out.println("Goodbye");
         System.out.println("Hi!");
-        System.out.println("hat geklappt 'lol' :D");
         System.out.println("test");
-
+        System.out.println("Goodbye");
+        System.out.println("hat geklappt 'lol' :D");
         System.out.println("??");
         System.out.println("git test");
+        System.out.println("Moin");
     }
-
 }
